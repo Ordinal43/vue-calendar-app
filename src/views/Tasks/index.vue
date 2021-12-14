4a3b4f91484851f69f3c6365d3f8935a1d7868ab
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2021-12-14 15:04:14 -0800

    did simple API call for tasks

--- src/views/Tasks/index.vue
+++ src/views/Tasks/index.vue
@@ -3,13 +3,24 @@
     class="fluid"
   >
     <h1>tasks</h1>
+    <div
+      v-for="(task, i) in tasks"
+      :key="`task-${i}`"
+    >
+      {{ i+1 }}. {{ task.title }}
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    tasks: []
-  })
+  computed: {
+    tasks () {
+      return this.$store.state.tasks;
+    }
+  },
+  mounted () {
+    this.$store.dispatch("fetchTasks");
+  }
 };
 </script>
